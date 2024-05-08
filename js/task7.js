1731c1d5ff91342fcb83877a15622319f98fc89f
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-05-08 19:00:40 +0300

    task 7

---
+++ js/task7.js
@@ -0,0 +1,16 @@
+function getBase64FromUrl(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      const reader = new FileReader();
+      reader.onloadend = function () {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(xhr.response);
+    };
+    xhr.onerror = reject;
+    xhr.open("GET", url);
+    xhr.responseType = "blob";
+    xhr.send();
+  });
+}
